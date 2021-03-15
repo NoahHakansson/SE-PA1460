3237320b0351e78b9ccd8f95466a25afa3e0b1d6
Dingus <77668883+DingusDungus@users.noreply.github.com>
2021-03-15 14:34:27 +0100

    Add files via upload

---
+++ unitTests/GameObject.cpp
@@ -0,0 +1,20 @@
+#include "GameObject.hpp"
+GameObject::GameObject(std::string name) {
+    objectName = name;
+    moveStrategy* movestrat = new moveStrategy;
+    pickUpStrategy* pickUpStrat = new pickUpStrategy;
+
+    interactionList.push_back(movestrat);
+    interactionList.push_back(pickUpStrat);
+}
+
+std::string GameObject::getObjectName() { return objectName; }
+
+std::string GameObject::listInteractionTypes() {
+    std::string returnList = "Interactions : ";
+    for (int i = 0;i < interactionList.size();i++) {
+        returnList += (interactionList[i]->getName() + ",");
+    }
+
+    return returnList;
+}
---
+++ unitTests/interactionTypes.hpp
@@ -0,0 +1,36 @@
+#include <string>
+#include <vector>
+
+class interactionType {
+protected:
+    std::vector<std::string> OptionList;
+public:
+    std::string virtual getName() = 0;
+};
+
+class pickUpStrategy : public interactionType {
+    protected: 
+        std::vector<std::string> OptionList = {"Gently", "Hard"};
+    public:
+        std::string getName() override {
+            return "Pick up";
+        }
+};
+
+class moveStrategy : public interactionType {
+    protected: 
+        std::vector<std::string> OptionList = {"Gently", "Hard", "Barbaric"};
+    public:
+        std::string getName() override {
+            return "Move it";
+        }
+};
+
+class dropStrategy : public interactionType {
+    protected: 
+        std::vector<std::string> OptionList = {"Gently", "Hard", "Nonchalant"};
+    public:
+        std::string getName() override {
+            return "Drop it";
+        }
+};
