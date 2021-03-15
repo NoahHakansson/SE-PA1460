021ede449145b47ac86975d03986e5d0d16c86b1
DingusDungus <gabriel.ivars01@protonmail.com>
2021-03-15 21:04:09 +0100

    Updated all of them

--- unitTests/interactionTypes.hpp
+++ unitTests/interactionTypes.hpp
@@ -2,13 +2,17 @@
 #include <vector>
 #include <iostream>
 
-class interactionType {
+class interactionType
+{
 protected:
-    std::vector<std::string> optionList;
+    std::vector<std::string> optionList = {};
     std::string option = "";
-    std::string optionConfirmation(std::string theOption) {
-        for (int i = 0; i < this->optionList.size(); i++) {
-            if (theOption == this->optionList[i]){
+    std::string virtual optionConfirmation(std::string theOption)
+    {
+        for (int i = 0; i < this->optionList.size(); i++)
+        {
+            if (theOption == this->optionList[i])
+            {
                 this->option = theOption;
                 return "SUCCESS";
             }
@@ -16,38 +20,48 @@ protected:
         std::cout << this->optionList.size();
         return "FAILED";
     }
+
 public:
     std::string virtual getName() = 0;
-    std::string setOption(std::string theOption){
+    std::string setOption(std::string theOption)
+    {
         std::string confirmationMessage = "";
         confirmationMessage = this->optionConfirmation(theOption);
         return confirmationMessage;
     }
 };
 
-class pickUpStrategy : public interactionType {
-    protected: 
-        std::vector<std::string> optionList = {"Gently", "Hard"};
-    public:
-        std::string getName() override {
-            return "Pick up";
-        }
+class pickUpStrategy : public interactionType
+{
+protected:
+    std::vector<std::string> optionList = {"Gently", "Hard"};
+
+public:
+    std::string getName() override
+    {
+        return "Pick up";
+    }
 };
 
-class moveStrategy : public interactionType {
-    protected: 
-        std::vector<std::string> optionList = {"Gently", "Hard", "Barbaric"};
-    public:
-        std::string getName() override {
-            return "Move it";
-        }
+class moveStrategy : public interactionType
+{
+protected:
+public:
+    moveStrategy() { optionList = {"Gently", "Hard", "Barbaric"}; }
+    std::string getName() override
+    {
+        return "Move it";
+    }
 };
 
-class dropStrategy : public interactionType {
-    protected: 
-        std::vector<std::string> optionList = {"Gently", "Hard", "Nonchalant"};
-    public:
-        std::string getName() override {
-            return "Drop it";
-        }
+class dropStrategy : public interactionType
+{
+protected:
+    std::vector<std::string> optionList = {"Gently", "Hard", "Nonchalant"};
+
+public:
+    std::string getName() override
+    {
+        return "Drop it";
+    }
 };
