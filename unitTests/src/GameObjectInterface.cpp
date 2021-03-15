230798bc5e2ae20157e91fc5a8e55ce404f3fb00
Noah <noah@pop-os.localdomain>
2021-03-15 15:14:05 +0100

    second try adding new directory structure

---
+++ unitTests/src/GameObjectInterface.cpp
@@ -0,0 +1,22 @@
+#include "../GameObjectInterface.hpp"
+
+GameObjectInterface::GameObjectInterface() {
+    GameObject ball("Ball");
+    gameobjects.push_back(ball);
+}
+
+std::string GameObjectInterface::selectGameObject(std::string theGameObject) {
+    bool selected = false;
+    for (int i = 0;i < gameobjects.size();i++) {
+        if (gameobjects[i].getObjectName() == theGameObject) {
+            CurrentObject = gameobjects[i];
+            selected = true;
+        }
+    }
+
+    if (selected == false) {
+        return "null";
+    }
+
+    return CurrentObject.listInteractionTypes();
+}
